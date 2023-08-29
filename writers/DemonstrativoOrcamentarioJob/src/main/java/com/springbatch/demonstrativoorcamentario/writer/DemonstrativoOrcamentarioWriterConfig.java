7ec7010faa9dd873563b7a41e292c1145e838b75
Natan Cavasin <natancavasin@hotmail.com>
2023-08-29 13:55:27 -0300

    project: DemonstrativoOrcamentarioJob feat: implementeMultiResourceItemWriter

--- writers/DemonstrativoOrcamentarioJob/src/main/java/com/springbatch/demonstrativoorcamentario/writer/DemonstrativoOrcamentarioWriterConfig.java
+++ writers/DemonstrativoOrcamentarioJob/src/main/java/com/springbatch/demonstrativoorcamentario/writer/DemonstrativoOrcamentarioWriterConfig.java
@@ -2,14 +2,17 @@ package com.springbatch.demonstrativoorcamentario.writer;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Date;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.file.FlatFileHeaderCallback;
 import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.MultiResourceItemWriter;
+import org.springframework.batch.item.file.ResourceSuffixCreator;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.builder.MultiResourceItemWriterBuilder;
 import org.springframework.batch.item.file.transform.LineAggregator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +25,33 @@ import com.springbatch.demonstrativoorcamentario.dominio.Lancamento;
 @Configuration
 public class DemonstrativoOrcamentarioWriterConfig {
 	
+	@StepScope
+	@Bean
+	public MultiResourceItemWriter<GrupoLancamento> multidemonstrativoOrcamentarioWriter(
+			@Value("#{jobParameters['demonstartivosOrcamentarios']}") Resource resource,
+			FlatFileItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter
+		){
+		return new MultiResourceItemWriterBuilder<GrupoLancamento>()
+				.name("multidemonstrativoOrcamentarioWriter")
+				.resource(resource)
+				.delegate(demonstrativoOrcamentarioWriter)
+				.resourceSuffixCreator(suffixCreator())
+				.itemCountLimitPerResource(1) //Cria um arquivo para cada grupo definido, deve ser consistente com o valor do chunk
+				.build();
+	}
+	
+	
+	private ResourceSuffixCreator suffixCreator() {
+		return new ResourceSuffixCreator() {
+			
+			@Override
+			public String getSuffix(int index) {
+				return index + ".txt";
+			}
+		};
+	}
+
+
 	@StepScope
 	@Bean
 	public FlatFileItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter(
