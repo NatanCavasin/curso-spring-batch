3d41dd5a91af1f91e8b8d350f6250805a8d837c3
Natan Cavasin <natancavasin@hotmail.com>
2023-08-29 15:04:24 -0300

    project: projContasBancariasJob feat: implemented CompositeContaWriterConfig

---
+++ writers/ContasBancariasJob/src/main/java/com/springbatch/contasbancarias/writer/CompositeContaWriterConfig.java
@@ -0,0 +1,24 @@
+package com.springbatch.contasbancarias.writer;
+
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.support.CompositeItemWriter;
+import org.springframework.batch.item.support.builder.CompositeItemWriterBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.contasbancarias.dominio.Conta;
+
+@Configuration
+public class CompositeContaWriterConfig {
+
+	@Bean
+	public CompositeItemWriter<Conta> compositeContaWriter(
+			FlatFileItemWriter<Conta> flatFileItemWriter,
+			JdbcBatchItemWriter<Conta> jdbcFileItemWriter
+	 ){
+		return new CompositeItemWriterBuilder<Conta>()
+				.delegates(flatFileItemWriter, jdbcFileItemWriter)
+				.build();
+	}
+}
