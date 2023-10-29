6009093bd666c145c049d4ae9b7ac29bf608feea
Natan Cavasin <natancavasin@hotmail.com>
2023-10-29 17:02:09 -0300

    feat: initial configuration

---
+++ Projects/EnvioPromocoesClientesJob/src/main/java/com/springbatch/enviopromocoesemail/step/EnvioemailClientesStepConfig.java
@@ -0,0 +1,36 @@
+package com.springbatch.enviopromocoesemail.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+
+import com.springbatch.enviopromocoesemail.dominio.InteresseProdutoCliente;
+
+@Configuration
+public class EnvioemailClientesStepConfig {
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Step envioemailClientesStep(
+			ItemReader<InteresseProdutoCliente> lerInteresseProdutoClienteProcessor,
+			ItemProcessor<InteresseProdutoCliente, SimpleMailMessage> processarEmailProdutoClienteProcessor,
+			ItemWriter<SimpleMailMessage> enviarEmailProdutoClienteWriter
+			) {
+		return stepBuilderFactory
+				.get("envioemailClientesStep")
+				.<InteresseProdutoCliente, SimpleMailMessage>chunk(1)
+				.reader(lerInteresseProdutoClienteProcessor)
+				.processor(processarEmailProdutoClienteProcessor)
+				.writer(enviarEmailProdutoClienteWriter)
+				.build();
+	}
+
+}
